00c298c79417bdb88437635b72734d13f9175688
querl369 <yemets3@gmail.com>
2021-01-05 19:43:13 +0200

    Rerun plugin added, conftest edited

--- stepik_env/fixtures/conftest.py
+++ stepik_env/fixtures/conftest.py
@@ -1,22 +1,29 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help='Choose browser: chrome or firefox')
+    parser.addoption('--language', action='store', default=None, help='Choose language etc"es/ru/ua/fr/de..."')
 
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+        print(f"\nstart chrome browser for test.. with {user_language} language")
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        print(f"\nstart firefox browser for test.. with {user_language} language")
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--user_language should be defined")
     yield browser
     print("\nquit browser..")
     browser.quit()
